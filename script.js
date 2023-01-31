e097bf22f6b3e150fb3b37fdf3faf3dab74f5c92
Gustav72 <wolfgangkirstein7@gmail.com>
2023-01-30 22:40:58 -0500

    remove switch

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const button = document.querySelector('button');
 const form = document.querySelector('form')
 const input = document.querySelector('input');
 const unitSwitch = document.querySelector('.switch')
+const container = document.querySelector('.weather-container')
 
 let cityName = 'boston';
 let units = 'imperial'
 
 async function findWeather(cityName) {
         
+try {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' 
     + cityName 
     + '&appid=e37d4ef1a74a626d446d214d6d697470' 
@@ -17,35 +19,50 @@ async function findWeather(cityName) {
       return response.json();
     })
     .then(function(response) {
-      console.log(response);
+      
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        let name = document.createElement('h2');
+        let temperature = document.createElement('p');
+        let weatherImg = document.createElement('img')
+        let weather = document.createElement('p');
+        let feelsLike = document.createElement('p');
+        let humidity = document.createElement('p')
+
+        name.innerHTML = response.name;
+        temperature.innerHTML = response.main.temp;
+        weatherImg.src = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+        weather.innerHTML = response.weather[0].description;
+        feelsLike.innerHTML = response.main.feels_like;
+        humidity.innerHTML = response.main.humidity;
+        
+
+
+        container.appendChild(name)
+        container.appendChild(temperature)
+        container.appendChild(weatherImg)
+        container.appendChild(weather)
+        container.appendChild(feelsLike)
+        container.appendChild(humidity)
+
+        console.log(response)
     });
-
-    console.log(units)
-
 }
+catch(error) {
+    alert(error);
+}
+}   
 
 button.addEventListener('click', function() {
     
     form.addEventListener('submit',  function(event) {
         event.preventDefault()
     });
-
-    console.log('hi')
     
     cityName = input.value;
 
     findWeather(cityName);
-
-});
-
-unitSwitch.addEventListener('click', function() {
-
-    if(units == 'imperial') {
-        units = 'metric'        
-    }
-    else {
-        units = 'imperial'
-    }
-    console.log(units)
 
 });
